801a98db25310e9114ad2bfb28562856d97b282f
Michael Ye <micxye@yahoo.com>
2018-11-14 23:53:48 -0800

    implement cell formatting functions

--- index.js
+++ index.js
@@ -2,6 +2,57 @@
 // get products of all prime numbers
 // generate and log table
 
+const logTable = n => {
+    // create first line of all primes, space in front
+    const primes = generateNPrimes(n);
+    printFirstRow(primes);
+    // console.log('   ' + primes.join('     ')); // prints top row, 5 spaces between each number
+    // add line divider
+    // create 2nd line
+}
+
+const printFirstRow = nums => {
+    const formattedRow = nums.map(num => firstRowCellFormatter(num))
+    // prints the first row, with all cells formatted
+    console.log('   ' + formattedRow.join(''));
+}
+
+const firstRowCellFormatter = num => {
+    // 2 cases: 1 digit
+    let str = num.toString();
+    if (str.length === 1) {
+        str = `  ${str}  `;
+    } else if (str.length === 2) {
+        str = ` ${str}  `;
+    }
+    return str;
+    // returns 5 character format for each num, spaces at end, e.g. __5__, _11__
+}
+
+const firstColumnCellFormatter = num => {
+    let str = num.toString();
+    if (str.length === 1) {
+        str = ` ${str}|`
+    } else if (str.length === 2) {
+        str = `${str}|`
+    }
+    return str;
+    // returns 3 character format with divider at end, e.g. 11|
+}
+
+const cellFormatter = num => {
+    let str = num.toString();
+    if (str.length === 1) {
+        str = `  ${str} |`
+    } else if (str.length === 2) {
+        str = ` ${str} |`
+    } else if (str.length === 3) {
+        str = ` ${str}|`
+    }
+    return str;
+    // returns 5 character format, divider at the end, space always in front, e.g __5_|, _10_|, _143|
+}
+
 const generateNPrimes = n => {
     const primes = [2];
     let i = 3;
@@ -17,9 +68,12 @@ const generateNPrimes = n => {
     return primes;
 }
 
-module.exports = {
-    generateNPrimes
-}
+logTable(10)
 // process.stdout.write('2384823842384hsdfhjasfhwhefakwhfwhfkjahfjkasjkefhajkwhfjkawefjkahwkfh');
 
-console.log(generateNPrimes(10))
+module.exports = {
+    generateNPrimes,
+    firstRowCellFormatter,
+    firstColumnCellFormatter,
+    cellFormatter
+}
--- test.js
+++ test.js
@@ -1,5 +1,41 @@
-const { generateNPrimes } = require('./index');
+const {
+    generateNPrimes,
+    firstRowCellFormatter,
+    firstColumnCellFormatter,
+    cellFormatter
+} = require('./index');
 
 test('generateNPrimes function should generate N number of prime numbers', () => {
     expect(generateNPrimes(20).length).toBe(20);
+});
+
+// tests for firstRowCellFormatter function
+test('A single digit number input should create a 5 char string with 2 spaces in front and back', () => {
+    expect(firstRowCellFormatter(1)).toBe('  1  ');
+});
+
+test('A double digit number input should create a 5 char string with 1 space in front and 2 spaces in back', () => {
+    expect(firstRowCellFormatter(10)).toBe(' 10  ');
+});
+
+// tests for firstColumnCellFormatter function
+test('A single digit number input should create a 3 char string with 1 space in front and a vertical bar at the end', () => {
+    expect(firstColumnCellFormatter(1)).toBe(' 1|');
+});
+
+test('A double digit number input should create a 3 char string with a vertical bar at the end', () => {
+    expect(firstColumnCellFormatter(10)).toBe('10|');
+});
+
+// test for cellFormatter function
+test('A single digit number input should create a 5 char string ending with a space and a vertical bar', () => {
+    expect(cellFormatter(1)).toBe('  1 |');
+});
+
+test('A double digit number input should create a 5 char string with the number in between 2 spaces and a vertical bar at the end', () => {
+    expect(cellFormatter(10)).toBe(' 10 |');
+});
+
+test('A triple digit number input should create a 5 char string with a space in front of the number and a vertical bar at the end', () => {
+    expect(cellFormatter(100)).toBe(' 100|');
 });
